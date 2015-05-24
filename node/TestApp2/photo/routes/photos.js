1c455467b314a0f79938374183fd23592acabe76
Wei Fang <lightningfany@foxmail.com>
2015-05-24 16:55:23 +0900

    photo: list

---
+++ node/TestApp2/photo/routes/photos.js
@@ -0,0 +1,16 @@
+var photos = [];
+photos.push({
+	name: 'Node.js logo',
+	path: 'http://nodejs.org/images/logos/nodejs-green.png'
+});
+photos.push({
+	name: 'Ryan Speaking',
+	path: 'http://nodejs.org/images/logos/ryan-speaker.jpg'
+});
+
+exports.list = function(req, res) {
+	res.render('photos', {
+		title: "Photos",
+		photos: photos
+	});
+};
